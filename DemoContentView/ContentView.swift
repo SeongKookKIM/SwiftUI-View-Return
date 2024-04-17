a25ab86443e0425b1623b34b0435b329ce10cdf4
SeongKookKIM <ksk8646@naver.com>
2024-04-17 13:36:35 +0900

    Clear

--- DemoContentView/ContentView.swift
+++ DemoContentView/ContentView.swift
@@ -8,19 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var fileOpen: Bool = true
+   
     var body: some View {
-        var myString: String = "File closed"
-        if(fileOpen) {
-            myString = "File open"
-        }
-        return VStack {
-            HStack {
-                Text(myString)
-                    .padding()
-                Text("Goodbye, Wordl")
-            }
-        }
+
     }
 }
 
