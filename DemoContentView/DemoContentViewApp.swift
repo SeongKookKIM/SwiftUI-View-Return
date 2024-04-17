a536b5f7a416a2a9364c508b4ccb33be8b27fd79
SeongKookKIM <ksk8646@naver.com>
2024-04-17 13:04:15 +0900

    Initial Commit

---
+++ DemoContentView/DemoContentViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoContentViewApp.swift
+//  DemoContentView
+//
+//  Created by SeongKook on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoContentViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
