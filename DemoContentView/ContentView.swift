b1cef00b48843d87688f68e4a0b048ec1af359c5
SeongKookKIM <ksk8646@naver.com>
2024-04-18 10:27:33 +0900

    Clear

--- DemoContentView/ContentView.swift
+++ DemoContentView/ContentView.swift
@@ -1,38 +1,11 @@
-//
-//  ContentView.swift
-//  DemoContentView
-//
-//  Created by SeongKook on 4/17/24.
-//
+
 
 import SwiftUI
-
-
-
-
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            HStack {
-                Image(systemName: "goforward.10")
-                Image(systemName: "goforward.15")
-                Image(systemName: "goforward.30")
-            }
-            .font(.largeTitle)
-            .padding()
-            
-            HStack(alignment: .top) {
-                Text("Q1 Sales")
-                    .font(.headline)
-                Spacer()
-                VStack(alignment: .leading) {
-                    Text("January")
-                    Text("Feb")
-                    Text("March")
-                }
-                Spacer()
-            }
+          
         }
     }
 }
