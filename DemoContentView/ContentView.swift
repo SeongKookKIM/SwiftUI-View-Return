3b74e53ddc92cbf923b5b45379b47f5e77e6247b
SeongKookKIM <ksk8646@naver.com>
2024-04-18 11:43:24 +0900

    State 상태관리

--- DemoContentView/ContentView.swift
+++ DemoContentView/ContentView.swift
@@ -3,10 +3,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State private var wifiEnabled = true
+    @State private var userName = ""
     var body: some View {
         VStack{
-            
+            Toggle(isOn: $wifiEnabled) {
+                Text("Enable WiFi")
+            }
+            TextField("Enter user name", text: $userName)
+                .border(Color.black)
+            Text(userName)
+            Image(systemName: wifiEnabled ? "wifi" : "wifi.slash")
+                .font(.largeTitle)
         }
     }
 }
