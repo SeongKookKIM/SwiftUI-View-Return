3a322ea786a209168678994f746a97c674dbc0c8
SeongKookKIM <ksk8646@naver.com>
2024-04-18 11:16:23 +0900

    Clear

--- DemoContentView/ContentView.swift
+++ DemoContentView/ContentView.swift
@@ -3,18 +3,10 @@
 import SwiftUI
 
 struct ContentView: View {
-  
+    
     var body: some View {
         HStack {
-            Image(systemName: "airplane")
-            Text("Flight times:")
-            Text("London")
-        }
-        .font(.largeTitle)
-        .frame(width: 300)
-        .lineLimit(1)
-        .transaction { transaction in
-            transaction.disablesAnimations = true
+            
         }
     }
 }
