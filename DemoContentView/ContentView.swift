47f8e3aacdb72a7ddcdbda8552894b4766ace206
SeongKookKIM <ksk8646@naver.com>
2024-04-18 11:33:08 +0900

    Clear

--- DemoContentView/ContentView.swift
+++ DemoContentView/ContentView.swift
@@ -5,16 +5,8 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                Text("Hello Wordl, How are you?")
-                    .padding()
-                    .font(.largeTitle)
-                    .border(Color.black)
-                //  .frame(minWidth: 100, maxWidth: 300, minHeight: 100, maxHeight: 100, alignment: .center)
-                    .frame(minWidth: 0, maxWidth: geometry.size.width / 2, minHeight: 0, maxHeight: .infinity, alignment: .center)
-            }
-            .ignoresSafeArea(.all)
+        VStack{
+            
         }
     }
 }
