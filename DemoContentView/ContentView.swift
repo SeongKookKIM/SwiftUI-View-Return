fde5b819a443a4d55eca0e33d79b4482cad11f94
SeongKookKIM <ksk8646@naver.com>
2024-04-18 09:28:09 +0900

    Button Action

--- DemoContentView/ContentView.swift
+++ DemoContentView/ContentView.swift
@@ -12,8 +12,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            
+            Button(action: buttonPressed) {
+                Text("Click Me")
+            }
         }
+    }
+    
+    func buttonPressed() {
+        print("Click")
     }
 }
 
