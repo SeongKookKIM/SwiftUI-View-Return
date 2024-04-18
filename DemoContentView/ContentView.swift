bfbb8333c414c2911a8d6cb8bfcb631a62bf81a4
SeongKookKIM <ksk8646@naver.com>
2024-04-18 09:42:11 +0900

    Custom Stack

--- DemoContentView/ContentView.swift
+++ DemoContentView/ContentView.swift
@@ -7,13 +7,36 @@
 
 import SwiftUI
 
+struct MyStack<Content: View>: View {
+    let content: () -> Content
+    
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            content()
+        }
+        .font(.largeTitle)
+    }
+}
+
 
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            
+        MyStack {
+            Text("Text Item 01")
+            Text("Text Item 02")
+            Text("Text Item 03")
+            HStack {
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star")
+            }
         }
+
     }
 }
 
