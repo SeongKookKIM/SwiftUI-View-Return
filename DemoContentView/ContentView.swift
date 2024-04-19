01462dc2454fd12cc94dda40f962185833bd6239
SeongKookKIM <ksk8646@naver.com>
2024-04-19 10:05:06 +0900

    Clear

--- DemoContentView/ContentView.swift
+++ DemoContentView/ContentView.swift
@@ -3,28 +3,14 @@
 import SwiftUI
 
 
-class DemoData: ObservableObject {
-    @Published var userCount = 0
-    
-    func increament() {
-        userCount += 1
-    }
-}
+
 
 struct ContentView: View {
-    // ObservedObject StateObject는 차이가 있음
-    @ObservedObject var demoData: DemoData = DemoData()
+
     
     var body: some View {
         VStack{
-            Text("you are user number \(demoData.userCount)")
-            
-            Button(action:{
-                demoData.increament()
-            }) {
-                Text("Increament")
-                    .font(.largeTitle)
-            }
+         
         }
     }
 }
