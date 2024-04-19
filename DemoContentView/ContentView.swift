803955203f628d17d1275a20d1960e4031a40387
SeongKookKIM <ksk8646@naver.com>
2024-04-19 10:21:03 +0900

    Animation, TextField, Slider

--- DemoContentView/ContentView.swift
+++ DemoContentView/ContentView.swift
@@ -6,11 +6,24 @@ import SwiftUI
 
 
 struct ContentView: View {
-
+    @State private var rotation : Double = 0
+    @State private var text : String = "Welcome"
     
     var body: some View {
         VStack{
-         
+            Text("Hello, World")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.purple)
+                .rotationEffect(.degrees(rotation))
+                .animation(.easeInOut(duration: 5), value: rotation)
+            
+            TextField("Enter Text Here", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Slider(value: $rotation, in: 0...360,step: 0.1)
+            
+            
         }
     }
 }
