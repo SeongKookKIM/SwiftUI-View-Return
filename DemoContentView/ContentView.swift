25515248dc21b19688fb6987fdd6e06a157f8d03
SeongKookKIM <ksk8646@naver.com>
2024-04-19 09:35:16 +0900

    Observered

--- DemoContentView/ContentView.swift
+++ DemoContentView/ContentView.swift
@@ -2,11 +2,27 @@
 
 import SwiftUI
 
+
+class DemoData: ObservableObject {
+    @Published var userCount = 0
+    @Published var currentUser = ""
+    
+    init() {
+        updateData()
+    }
+    
+    func updateData() {
+        
+    }
+}
+
 struct ContentView: View {
-  
+    // ObservedObject StateObject는 차이가 있음
+    @ObservedObject var demoData: DemoData = DemoData()
+    
     var body: some View {
         VStack{
-            
+            Text("\(demoData.currentUser), you are user number \(demoData.userCount)")
         }
     }
 }
