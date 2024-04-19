41cc843085c32087d24fbad4fb080d0eb92cdfc3
SeongKookKIM <ksk8646@naver.com>
2024-04-19 09:26:31 +0900

    Clear

--- DemoContentView/ContentView.swift
+++ DemoContentView/ContentView.swift
@@ -3,28 +3,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wifiEnabled = true
-    @State private var userName = ""
+  
     var body: some View {
         VStack{
-            Toggle(isOn: $wifiEnabled) {
-                Text("Enable WiFi")
-            }
-            TextField("Enter user name", text: $userName)
-                .border(Color.black)
-            Text(userName)
-            WifiImageView(wifiEnabled: $wifiEnabled)
+            
         }
     }
 }
 
-struct WifiImageView: View {
-    @Binding var wifiEnabled: Bool
-    var body: some View {
-        Image(systemName: wifiEnabled ? "wifi" : "wifi.slash")
-            .font(.largeTitle)
-    }
-}
+
 
 
 #Preview {
