48f32ae94a6f200305c3b4256421d4cc8c1bbfde
SeongKookKIM <ksk8646@naver.com>
2024-04-19 13:27:18 +0900

    Task detached

--- DemoContentView/ContentView.swift
+++ DemoContentView/ContentView.swift
@@ -16,17 +16,28 @@ struct ContentView: View {
     }
     func doSomething() {
         print("Start \(Date())")
-        Task{
-            async let result = takesTooLong()
+        // detached 작업을 분리하는 작업
+        let detachedTask = Task.detached(priority: .background) {
+            let parentIndex: Int = 3
+             async let result = takesTooLong(index: parentIndex)
             print("Date result: \(await result)")
+        }
+        
+        /*
+         Task.detached를 사용하여 생성된 Task는 주로 부모 Task와 별개로 실행되는 독립적인 작업에 사용됩니다. 이 작업은 부모 Task의 취소 여부와 상관없이 실행됩니다. 때문에 detachedTask.isCancelled이 true가 되는 경우는 없습니다.
+         만약 Task를 생성하고 즉시 취소하고자 한다면, 이를 위해서는 취소 가능한 Task를 생성하고 cancel 메서드를 호출해야 합니다.
+         */
+        if(!detachedTask.isCancelled) {
+            detachedTask.cancel()
         }
         
         print("End \(Date())")
     }
     
-    func takesTooLong() async -> Date {
+    func takesTooLong(index: Int) async -> Date {
+        print("tasksTooLong Start")
         sleep(5)
-        print("Async task completed at \(Date())")
+        print("Async task completed at \(index)")
         return Date()
     }
 }
