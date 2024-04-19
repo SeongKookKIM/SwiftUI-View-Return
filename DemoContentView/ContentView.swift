9ab6d039fa3b7cddc4e26a32cdb480c19024a205
SeongKookKIM <ksk8646@naver.com>
2024-04-19 14:38:54 +0900

    GithubService actor

--- DemoContentView/ContentView.swift
+++ DemoContentView/ContentView.swift
@@ -8,11 +8,37 @@ struct Repository: Codable, Identifiable {
     let description: String?
 }
 
+struct User: Codable {
+    let avatar_url: URL
+    let login: String
+}
+
+
+// Class 사용자와 다른 참조값 - 비동기 처리로 한 개씩 처리를 해줌 즉 -> 비동기를 스택 안에서 동기식으로 처리
+actor GithubService {
+    
+    func fetchUser(username: String) async throws -> User {
+        let url = URL(string: "https://api.github.com/users/\(username)")!
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode(User.self, from: data)
+    }
+    
+    func fetchReopsitories(username:String) async throws -> [Repository] {
+        let url = URL(string: "https://api.github.com/users/\(username)/repos")!
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        return try JSONDecoder().decode([Repository].self, from: data)
+    }
+}
+
 
 struct ContentView: View {
-    @State private var username = "SK"
+    @State private var username = "SeongKookKIM"
+    @State private var user: User?
     @State private var repositories: [Repository] = []
     @State private var status = "Fetch Data"
+    
+    let githubService = GithubService()
     
     var body: some View {
         VStack{
@@ -24,7 +50,10 @@ struct ContentView: View {
                 Task {
                     do {
                         status = "Loading"
-                        async let fetchRepositoris = fetchReopsitories(username: username)
+                        async let fetchUser = githubService.fetchUser(username: username)
+                        async let fetchRepositoris = githubService.fetchReopsitories(username: username)
+                        
+                        user = try await fetchUser
                         repositories = try await fetchRepositoris
                         status = "Fetch Data"
                         
@@ -33,6 +62,24 @@ struct ContentView: View {
                     }
                 }
             }
+            
+            if let user = user {
+                AsyncImage(url: user.avatar_url) {image in
+                    image.resizable()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: 100, height: 100)
+                .clipShape(Circle())
+                
+                Text(user.login)
+                    .font(.title)
+            }
+            
+            if(repositories.count != 0) {
+                Text("\(repositories.count)개")
+            }
+            
             
             List(repositories) {repo in
                 VStack(alignment: .leading) {
@@ -48,12 +95,7 @@ struct ContentView: View {
         }
     }
     
-    func fetchReopsitories(username:String) async throws -> [Repository] {
-        let url = URL(string: "https://api.github.com/users/\(username)/repos")!
-        let (data, _) = try await URLSession.shared.data(from: url)
-        
-        return try JSONDecoder().decode([Repository].self, from: data)
-    }
+  
 }
 
 
