c7a861319ca2835bfec56f25231a95d63ee72c37
SeongKookKIM <ksk8646@naver.com>
2024-04-19 11:35:52 +0900

    Async-Await

--- DemoContentView/ContentView.swift
+++ DemoContentView/ContentView.swift
@@ -3,28 +3,29 @@
 import SwiftUI
 
 
-
-
 struct ContentView: View {
-    @State private var rotation : Double = 0
-    @State private var text : String = "Welcome"
     
     var body: some View {
         VStack{
-            Text("Hello, World")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundColor(Color.purple)
-                .rotationEffect(.degrees(rotation))
-                .animation(.easeInOut(duration: 5), value: rotation)
-            
-            TextField("Enter Text Here", text: $text)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            Slider(value: $rotation, in: 0...360,step: 0.1)
-            
-            
+            Button(action:{
+                doSomething()
+            }) {
+                Text("Do something")
+            }
         }
+    }
+    func doSomething() {
+        print("Start \(Date())")
+        Task{
+            await takesTooLong()
+        }
+        
+        print("End \(Date())")
+    }
+    
+    func takesTooLong() async {
+        sleep(5)
+        print("Async task completed at \(Date())")
     }
 }
 
